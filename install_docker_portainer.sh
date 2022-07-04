a944cf5048955459a59faa2ea4dcbe00ac17d4ac
Cerbrus <Cerbrus@users.noreply.github.com>
2022-07-04 23:07:01 +0200

    Update install_docker_portainer.sh

--- install_docker_portainer.sh
+++ install_docker_portainer.sh
@@ -26,4 +26,4 @@ sudo systemctl enable docker
 
 # Install Portainer with Docker on Linux
 # https://docs.portainer.io/start/install/server/docker/linux
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
