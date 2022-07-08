6e0f6e0978bc92006803bf1d6bf02f8e0f0ecb7a
Cerbrus <Cerbrus@users.noreply.github.com>
2022-07-08 23:06:40 +0200

    Added local IP

--- sh/discord_webhook_announce.sh
+++ sh/discord_webhook_announce.sh
@@ -3,10 +3,14 @@
 cd /home/pi/sh
 
 IP=$(curl ifconfig.me)
+
+IP_L=$(hostname -I)
+IP_L=${IP_L%% *}
+
 HOSTNAME=$(hostname)
 
 WEBHOOK=$(cat ./discord-webhook.url)
-MESSAGE="A Pi just came online.\n> **Hostname**:\n> $HOSTNAME\n> **IP**:\n> $IP\n"
+MESSAGE="A Pi just came online.\n> **Hostname**:\n> \`$HOSTNAME\`\n> **IP**:\n> \`$IP\` (external)\n> \`$IP_L\` (local)\n"
 curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"$MESSAGE\"}" $WEBHOOK
 
 exit 0
