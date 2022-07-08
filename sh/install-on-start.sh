69c97422f84e561e1cbcb5fc56cf46a898d7f434
Cerbrus <Cerbrus@users.noreply.github.com>
2022-07-08 21:59:50 +0200

    idiot-proofing

--- sh/install-on-start.sh
+++ sh/install-on-start.sh
@@ -5,7 +5,7 @@ GH_FOLDER=https://raw.githubusercontent.com/Cerbrus/pi/master
 
 # prepare dir.
 echo 1. Preparing script directory.
-mkdir $HOME/sh
+mkdir -p $HOME/sh
 
 # Store webhook
 echo 2. Storing webhook.
@@ -17,14 +17,14 @@ echo $WEBHOOK > sh/discord-webhook.url
 # Download Shell scripts
 echo 3. Downloading shell scripts.
 
-wget -q $GH_FOLDER/sh/on-start.sh -P $HOME/
-wget -q $GH_FOLDER/sh/discord_webhook_announce.sh -P $HOME/sh/
+wget -q -O $GH_FOLDER/sh/on-start.sh -P $HOME/
+wget -q -O $GH_FOLDER/sh/discord_webhook_announce.sh -P $HOME/sh/
 sudo chmod +x $HOME/on-start.sh
 sudo chmod +x $HOME/sh/on-start.sh
 
 # Install service
 echo 3. Downloading and installing on-start service.
 
-sudo wget -q $GH_FOLDER/services/on-start.service -P /etc/systemd/system/
+sudo wget -q -O $GH_FOLDER/services/on-start.service -P /etc/systemd/system/
 sudo systemctl enable on-start.service
 sudo systemctl status on-start.service
