1a98d06929f74e32e85cde19710ebb12fce1a861
Cerbrus <Cerbrus@users.noreply.github.com>
2022-07-08 22:47:49 +0200

    Also start the service

--- sh/install-on-start.sh
+++ sh/install-on-start.sh
@@ -37,4 +37,5 @@ sudo wget -q $GH_FOLDER/services/on-start.service -O /etc/systemd/system/on-star
 sudo sed -i "s/HOMEDIR/\/home\/$USER/g" /etc/systemd/system/on-start.service
 
 sudo systemctl enable on-start.service
+sudo systemctl start on-start.service
 sudo systemctl status on-start.service
