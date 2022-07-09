0e5ea9ed63b19c66d24584ab636b070cc8ded67e
Cerbrus <Cerbrus@users.noreply.github.com>
2022-07-09 10:50:17 +0200

    Readable text, added timestamp and username

--- sh/discord_webhook_announce.sh
+++ sh/discord_webhook_announce.sh
@@ -1,16 +1,31 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 cd /home/pi/sh
 
-IP=$(curl ifconfig.me)
+IP_EXTERNAL=$(curl ifconfig.me)
 
-IP_L=$(hostname -I)
-IP_L=${IP_L%% *}
+IP_LOCAL=$(hostname -I)
+IP_LOCAL=${IP_LOCAL%% *}
 
 HOSTNAME=$(hostname)
-
+TIMESTAMP=$(date +"_%Y-%m-%d - %T_")
 WEBHOOK=$(cat ./discord-webhook.url)
-MESSAGE="A Pi just came online.\n> **Hostname**:\n> \`$HOSTNAME\`\n> **IP**:\n> \`$IP\` (external)\n> \`$IP_L\` (local)\n"
-curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"$MESSAGE\"}" $WEBHOOK
+
+MESSAGE="
+$TIMESTAMP
+I just came online.
+> **Hostname**:
+> \`$HOSTNAME\`
+> **IP**:
+> \`$IP_EXTERNAL\` (external)
+> \`$IP_LOCAL\` (local)
+"
+
+JSON="{
+\"content\":\"${MESSAGE//$'\n'/\\n}\",
+\"username\":\"$HOSTNAME\"
+}"
+
+curl -X POST -H "Content-Type: application/json" -d "${JSON//$'\n'}" $WEBHOOK
 
 exit 0
